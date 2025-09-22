2d2e9e6377df44e5e162cb0c1702c210e1336d1a
DucKyWay <niji2549@gmail.com>
2025-09-22 14:54:53 +0700

    ui: creeate admin navbar template

---
+++ src/main/java/ku/cs/controllers/components/AdminNavbarController.java
@@ -0,0 +1,45 @@
+package ku.cs.controllers.components;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import ku.cs.components.Icons;
+import ku.cs.components.button.ElevatedButtonWithIcon;
+import ku.cs.services.FXRouter;
+
+import java.io.IOException;
+
+public class AdminNavbarController {
+    @FXML private Button manageOfficersButton;
+    @FXML private Button manageUsersButton;
+
+    @FXML public void initialize() {
+        initUserInterfaces();
+        initEvents();
+    }
+
+    private void initUserInterfaces() {
+        ElevatedButtonWithIcon.SMALL.mask(manageOfficersButton, Icons.EDIT);
+        ElevatedButtonWithIcon.SMALL.mask(manageUsersButton, Icons.EDIT);
+    }
+
+    private void initEvents() {
+        manageOfficersButton.setOnAction(e -> onManageOfficersButtonClick());
+        manageUsersButton.setOnAction(e -> onManageUsersButtonClick());
+    }
+
+    protected void onManageOfficersButtonClick() {
+        try {
+            FXRouter.goTo("admin-manage-officers");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected void onManageUsersButtonClick() {
+        try {
+            FXRouter.goTo("admin-manage-users");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
