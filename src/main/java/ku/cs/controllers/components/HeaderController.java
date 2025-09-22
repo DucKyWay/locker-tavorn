9938307ccb739ed70a90dddf36b93cdfe8f52e06
DucKyWay <niji2549@gmail.com>
2025-09-22 14:12:39 +0700

    ui: crate header template

---
+++ src/main/java/ku/cs/controllers/components/HeaderController.java
@@ -0,0 +1,12 @@
+package ku.cs.controllers.components;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class HeaderController {
+    @FXML private Button changeThemeButton;
+
+    @FXML public void initialize() {
+
+    }
+}
