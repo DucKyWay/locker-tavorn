d3871b84747d68a3e8023d8974d075514d8c4185
DucKyWay <niji2549@gmail.com>
2025-09-26 09:23:49 +0700

    fix: module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -25,7 +25,6 @@ module ku.cs.project681 {
     opens ku.cs.controllers.user to javafx.fxml;
     opens ku.cs.controllers.locker to javafx.fxml;
     opens ku.cs.models.request to jakarta.json.bind, javafx.base, org.eclipse.yasson;
-    opens ku.cs.models.request.date to jakarta.json.bind, javafx.base, org.eclipse.yasson;
     opens ku.cs.controllers.officer.DialogPane to javafx.fxml;
 
     exports ku.cs.project681;
@@ -39,6 +38,5 @@ module ku.cs.project681 {
     exports ku.cs.models.zone;
     exports ku.cs.models.account;
     exports ku.cs.models.request;
-    exports ku.cs.models.request.date;
     exports ku.cs.controllers.officer.DialogPane;
 }
